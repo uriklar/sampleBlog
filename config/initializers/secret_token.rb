05304b618345fe2030757b331902c753178300e7
Uri Klar <uriklar@gmail.com>
2012-11-29 12:21:03 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WazeEx::Application.config.secret_token = 'e2ab3180ec558907eb6f004ad4e9655bf542e332b70d4859126c78a282d683cb51a9ba7811f4599c86b291c9624310cc401dde11f47542e6f405569be740b1ce'
