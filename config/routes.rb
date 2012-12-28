a9088eaa9ad6fbf5e56986d2ceb4c51af438374e
Uri Klar <uriklar@gmail.com>
2012-12-29 01:14:09 +0200

    reached 7 in the guide

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 WazeEx::Application.routes.draw do
+  resources :posts
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ WazeEx::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
@@ -56,5 +60,5 @@ WazeEx::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  root  :to => 'welcome#index'
+  root  :to => 'home#index'
 end
